60b74eb06725c009f9baa0c8080cee3de4967f71
Atsuya Tanaka <tenmusu007@gmail.com>
2022-03-16 16:47:07 -0700

    update today's recipe

--- Javascript/atsu.js
+++ Javascript/atsu.js
@@ -518,27 +518,35 @@ const recipe = document.querySelector('.reco-recipe-sent')
 const recipe1 = document.querySelector('.reco-recipe-sent1')
 const recipe2 = document.querySelector('.reco-recipe-sent2')
 const recipe3 = document.querySelector('.reco-recipe-sent3')
-recipe.classList.add('none')
-recipe1.classList.add('none')
-recipe2.classList.add('none')
-recipe3.classList.add('none')
+const btnEl = document.querySelector('.btn')
+const btn1El = document.querySelector('.btn1')
+const btn2El = document.querySelector('.btn2')
+const btn3El = document.querySelector('.btn3')
+btnEl.classList.add('none')
+btn1El.classList.add('none')
+btn2El.classList.add('none')
+btn3El.classList.add('none')
 
-function change(){
 recipe.classList.add('none')
 recipe1.classList.add('none')
 recipe2.classList.add('none')
 recipe3.classList.add('none')
-    const picEL = document.getElementById('pic-today');
-    const images = ['Images/home-pic/chicken-home.jpg','Images/home-pic/tacos.jpg','images/home-pic/doner.jpg','Images/home-pic/coro-meal.jpg']
-    const imageNo = Math.floor( Math.random() * images.length)
-    if(imageNo == 0){
-        recipe.classList.remove('none')
-    }else if(imageNo == 1){
-        recipe1.classList.remove('none')
-    }else if(imageNo == 2){
-        recipe2.classList.remove('none')
-    }else if(imageNo == 3){
-        recipe3.classList.remove('none')
-    }
-    picEL.src = images[imageNo];
+const picEL = document.getElementById('pic-today');
+const images = ['Images/home-pic/chicken-home.jpg', 'Images/home-pic/tacos.jpg', 'images/home-pic/doner.jpg', 'Images/home-pic/coro-meal.jpg']
+const imageNo = Math.floor(Math.random() * images.length)
+if (imageNo == 0) {
+    recipe.classList.remove('none')
+    btnEl.classList.remove('none')
+} else if (imageNo == 1) {
+    recipe1.classList.remove('none')
+    btn1El.classList.remove('none')
+} else if (imageNo == 2) {
+    recipe2.classList.remove('none')
+    btn2El.classList.remove('none')
+} else if (imageNo == 3) {
+    recipe3.classList.remove('none')
+    btn3El.classList.remove('none')
 }
+picEL.src = images[imageNo];
+
+$("reco-recipe").fadeOut("slow");
